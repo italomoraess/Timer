5268df106d2a5eb5e088d3196a7c21d8384cc069
italomoraess <italomoraes.sth@gmail.com>
2023-11-22 20:13:26 -0300

    add styles page history

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,47 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu historico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>concluido</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
